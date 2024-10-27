f066ebe643bea00e6e28cd029c4b311ae570109e
Aravind <aravind6788@gmail.com>
2024-10-27 13:56:20 +0530

    updated

--- python/translate_server.py
+++ python/translate_server.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from googletrans import Translator
-import os  # Import the os module
+import os
 
 app = Flask(__name__)
-CORS(app)
+# Allow requests from your frontend application
+CORS(app, origins=["http://127.0.0.1:5500", "https://groupmeet-kd1o.onrender.com"])  # Add your production URL
+
 translator = Translator()
 
 @app.route('/translate', methods=['POST'])
@@ -25,6 +27,5 @@ def translate():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable
-    app.run(host='0.0.0.0', port=port)  # Bind to 0.0.0.0
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
